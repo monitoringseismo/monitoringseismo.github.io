d1c8f015ee303e0605220958192b13093f4fae49
Trio Pambudi Utomo <triopambudiutomo@gmail.com>
2025-10-09 15:56:20 +0700

    feat: custom button icon

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -12,7 +12,7 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 _flutter.loader.load({
   entrypointUrl: "/main.dart.js",
   serviceWorker: {
-    serviceWorkerVersion: "3001814583",
+    serviceWorkerVersion: "2880622645",
     serviceWorkerUrl: "/flutter_service_worker.js?v=",
   },
   onEntrypointLoaded: function(engineInitializer) {
