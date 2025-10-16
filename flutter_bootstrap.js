b078c15e460d78f3940544bd6f47a2b36fcd0c81
Trio Pambudi Utomo <triopambudiutomo@gmail.com>
2025-10-16 13:20:25 +0700

    fix: row

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -12,7 +12,7 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 _flutter.loader.load({
   entrypointUrl: "/main.dart.js",
   serviceWorker: {
-    serviceWorkerVersion: "401043576",
+    serviceWorkerVersion: "2919172562",
     serviceWorkerUrl: "/flutter_service_worker.js?v=",
   },
   onEntrypointLoaded: function(engineInitializer) {
